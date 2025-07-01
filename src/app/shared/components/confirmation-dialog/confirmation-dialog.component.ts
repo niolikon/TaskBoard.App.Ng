939b985683f568b56e8fed2da9b105849074b964
Simone Andrea Muscas <simoneandrea.muscas@gmail.com>
2025-07-01 19:53:47 +0200

    Set default focus on cancel confirmation dialog
      TBA4

--- src/app/shared/components/confirmation-dialog/confirmation-dialog.component.ts
+++ src/app/shared/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,7 +1,7 @@
-import { Component, Inject } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Inject, ViewChild} from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef, MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
-import { TranslatePipe } from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-confirmation-dialog',
@@ -9,15 +9,21 @@ import { TranslatePipe } from '@ngx-translate/core';
   imports: [
     MatDialogModule,
     MatButtonModule,
-    TranslatePipe
+    TranslateModule
   ],
   styleUrl: './confirmation-dialog.component.scss'
 })
-export class ConfirmationDialogComponent {
+export class ConfirmationDialogComponent implements AfterViewInit {
+  @ViewChild('cancelButton') cancelButton!: ElementRef<HTMLButtonElement>;
+
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: { title: string, message: string },
     private readonly dialogRef: MatDialogRef<ConfirmationDialogComponent>
   ) {}
+
+  ngAfterViewInit(): void {
+    this.cancelButton.nativeElement.focus();
+  }
 
   onConfirm(): void {
     this.dialogRef.close(true);
