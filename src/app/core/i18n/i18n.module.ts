88b6dee0951cbe82d5e4904ce8069d0c14ad77b3
Simone Andrea Muscas <simoneandrea.muscas@gmail.com>
2025-08-23 15:53:30 +0200

    Add support for locale management
      TBA8

--- src/app/core/i18n/i18n.module.ts
+++ src/app/core/i18n/i18n.module.ts
@@ -1,14 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { I18N_DEFAULT_LANGUAGE } from './i18n.config';
+import { I18N_DEFAULT_LANGUAGE, I18N_CORE_TRANSLATIONS_PATH  } from './i18n.config';
 import { LanguageSelectionService } from './services/language-selection.service';
 import { FeatureLocalizationLoaderService } from './services/feature-localization-loader.service';
+import { LocaleResolverService } from './services/locale-resolver.service';
 import { forkJoin } from 'rxjs';
 
 export function httpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './i18n/core/', '.json');
+  return new TranslateHttpLoader(http, I18N_CORE_TRANSLATIONS_PATH, '.json');
+}
+
+export function localeIdFactory(resolver: LocaleResolverService): string {
+  return resolver.locale ?? 'en-US';
 }
 
 @NgModule({
@@ -22,15 +27,22 @@ export function httpLoaderFactory(http: HttpClient) {
     })
   ],
   exports: [TranslateModule],
+  providers: [
+    { provide: LOCALE_ID, useFactory: localeIdFactory, deps: [LocaleResolverService] }
+  ]
 })
 export class CoreI18nModule {
   constructor(
     languageSelectionService: LanguageSelectionService,
     translate: TranslateService,
-    featureLocalizationLoaderService: FeatureLocalizationLoaderService
+    featureLocalizationLoaderService: FeatureLocalizationLoaderService,
+    localeResolverService: LocaleResolverService
   ) {
-    const browserLanguage = translate.getBrowserLang() ?? I18N_DEFAULT_LANGUAGE;
     translate.setDefaultLang(I18N_DEFAULT_LANGUAGE);
+    localeResolverService.init();
+
+    const browserLanguage = languageSelectionService.normalizeToSupported(translate.getBrowserLang())
+      ?? I18N_DEFAULT_LANGUAGE;
 
     forkJoin([
       featureLocalizationLoaderService.preloadCoreLocalizations(),
